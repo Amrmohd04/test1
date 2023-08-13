f20000c1e05c51fc3d7b757e9c1f7deed853433d
Amrmohd04 <141871548+Amrmohd04@users.noreply.github.com>
2023-08-14 02:02:42 +0400

    Create skindiseases.py

---
+++ skindiseases.py
@@ -0,0 +1,10 @@
+# skin_disease_data.py
+
+skin_diseases = [
+    {
+        "name": "Skin Rash",
+        "symptoms": ["rash", "redness", "itchiness"],
+        "description": "A skin rash is a noticeable change in the texture or color of the skin."
+    },
+    # Add more disease entries as needed
+]
